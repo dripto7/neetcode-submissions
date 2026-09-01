a5ac4e584765be040ebf3ff781c408fb0dff5e3a
DRIPTO <raydripto@gmail.com>
2026-09-02 02:49:26 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int arr[] = new int[2*nums.length];
+        for(int i = 0; i<nums.length;i++){
+            arr[i] = nums[i]; 
+        }
+        int index = nums.length;
+        for(int i = 0; i<nums.length;i++){
+            arr[index] = nums[i];
+            index++;
+             }
+             return arr;
+
+        
+    }
+}
